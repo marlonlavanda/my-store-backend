58f33f57ac00f8a3a9eeec312f33b0159c2d2318
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-11-15 23:52:17 +0000

    avances del curso

--- services/product.service.js
+++ services/product.service.js
@@ -1,6 +1,8 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
+const sequelize = require('../libs/sequelize');
+
 class ProductsService {
 
   constructor(){
@@ -10,14 +12,14 @@ class ProductsService {
 
   generate() {
     const limit = 100;
-    for(let i = 0; i < limit; i++){
+    for (let index = 0; index < limit; index++) {
       this.products.push({
         id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
         image: faker.image.imageUrl(),
-        isBlock: faker.datatype.boolean()
-      })
+        isBlock: faker.datatype.boolean(),
+      });
     }
   }
 
@@ -30,21 +32,19 @@ class ProductsService {
     return newProduct;
   }
 
-  find() {
-    return new Promise((reolve, reject) => {
-      setTimeout(() => {
-        reolve(this.products);
-      }, 3000);
-    })
+  async find() {
+    const query = 'SELECT * FROM tasks';
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
-    const product = this.products.find( item => item.id === id);
-    if(!product) {
-      throw boom.notFound('Product Not Found');
+    const product = this.products.find(item => item.id === id);
+    if (!product) {
+      throw boom.notFound('product not found');
     }
-    if(product.isBlock) {
-      throw boom.conflict('Product Is Block');
+    if (product.isBlock) {
+      throw boom.conflict('product is block');
     }
     return product;
   }
@@ -52,7 +52,7 @@ class ProductsService {
   async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
-      throw boom.notFound('Product Not Found');
+      throw boom.notFound('product not found');
     }
     const product = this.products[index];
     this.products[index] = {
@@ -63,13 +63,14 @@ class ProductsService {
   }
 
   async delete(id) {
-    const index = this.products.findIndex( item => item.id === id);
-    if(index === -1) {
-      throw boom.notFound('Product Not Found');
+    const index = this.products.findIndex(item => item.id === id);
+    if (index === -1) {
+      throw boom.notFound('product not found');
     }
     this.products.splice(index, 1);
     return { id };
   }
+
 }
 
-module.exports = ProductsService
+module.exports = ProductsService;
