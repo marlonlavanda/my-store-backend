4ec99a1169f5efcd148bf97bff7a80fc5528509d
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-11-17 21:57:06 +0000

    migraciones

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes')
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler')
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -22,6 +22,7 @@ app.use(cors(options));
 routerApi(app)
 
 app.use(logErrors);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
