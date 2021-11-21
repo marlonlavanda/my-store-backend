fb0283a59717f702bace558a0257e21c5dc90386
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-11-21 13:34:11 +0000

    Relations 1 to 1

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 const config = {
-  env: process.env.NODE_ENV || 'dev',
+  env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
+  dbUser: process.env.DB_USER || 'marlon',
+  dbPassword: process.env.DB_PASSWORD || 'admin123',
+  dbHost: process.env.DB_HOST || 'localhost',
+  dbName: process.env.DB_NAME || 'my_store',
+  dbPort: process.env.DB_PORT || '5432',
 }
 
 module.exports = { config }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const productsRouter = require('./products.router');
 const categoriesRouter = require('./categories.router');
 const usersRouter = require('./users.router');
+const customersRouter = require('./customers.router');
 const orderRouter = require('./orders.router');
 
 function routerApi(app) {
@@ -11,6 +12,7 @@ function routerApi(app) {
   router.use('/products', productsRouter);
   router.use('/categories', categoriesRouter);
   router.use('/users', usersRouter);
+  router.use('/customers', customersRouter);
   router.use('/orders', orderRouter);
 }
 
