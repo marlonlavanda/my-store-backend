d152960c2b662cd8de85d1760207ff989b2bc285
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-11-27 20:44:03 +0000

    consultas

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const productsRouter = require('./products.router');
 const categoriesRouter = require('./categories.router');
 const usersRouter = require('./users.router');
-const customersRouter = require('./customers.router');
 const orderRouter = require('./orders.router');
+const customersRouter = require('./customers.router');
 
 function routerApi(app) {
   const router = express.Router();
@@ -12,8 +12,8 @@ function routerApi(app) {
   router.use('/products', productsRouter);
   router.use('/categories', categoriesRouter);
   router.use('/users', usersRouter);
-  router.use('/customers', customersRouter);
   router.use('/orders', orderRouter);
+  router.use('/customers', customersRouter);
 }
 
 module.exports = routerApi;
--- routes/orders.router.js
+++ routes/orders.router.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const OrderService = require('../services/order.service');
+const { validatorHandler } = require('../middlewares/validator.handler');
+const { createOrderSchema, getOrderSchema, addItemSchema } = require('../schemas/order.schema');
 
 const router = express.Router();
+const service = new OrderService();
 
-router.get('/', (req, res) => {
-  res.json([]);
-});
+router.get('/:id',
+  validatorHandler(getOrderSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const order = await service.findOne(id);
+      res.json(order);
+    } catch(err){
+      next(err);
+  }
+})
+
+router.post('/',
+  validatorHandler(createOrderSchema, 'body'),
+  async (req, res) => {
+    const body = req.body;
+    const newOrder = await service.create(body);
+    res.status(201).json(newOrder)
+  }
+);
+
+router.post(
+  '/add-item',
+  validatorHandler(addItemSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newItem = await service.addItem(body);
+      res.status(201).json(newItem);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
