51195f9c2aded14d7e2fcfdb54dc0ccd284384a5
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-11-28 15:16:53 +0000

    configurations for deployment

--- config/config.js
+++ config/config.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 
 const config = {
   env: process.env.NODE_ENV || 'development',
+  isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
   dbUser: process.env.DB_USER || 'marlon',
   dbPassword: process.env.DB_PASSWORD || 'admin123',
   dbHost: process.env.DB_HOST || 'localhost',
   dbName: process.env.DB_NAME || 'my_store',
   dbPort: process.env.DB_PORT || '5432',
+  dbUrl: process.env.DATABASE_URL,
 }
 
 module.exports = { config }
